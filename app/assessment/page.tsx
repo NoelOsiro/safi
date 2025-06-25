624d906fb68a7088001741d85c143254c8283cdd
NoelOsiro <noel.osiro@azubiafrica.org>
2025-06-25 09:29:08 +0300

    removed server from assessment

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -1,10 +1,9 @@
 "use client"
-import { createClient } from "@/lib/supabase/server"
+import { supabase } from "@/lib/supabase/client"
 import { redirect } from "next/navigation"
 import AssessmentContent from "./assessment-content"
 
 export default async function AssessmentPage() {
-  const supabase = await createClient()
   const {
     data: { session },
   } = await supabase.auth.getSession()
